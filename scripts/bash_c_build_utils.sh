3e2681ce72300c8a034568cf155dca7303b86e83
Gary Woodcock <gary.woodcock@unthinkable.com>
2021-07-16 10:03:53 -0500

    Interim updates

--- scripts/bash_c_build_utils.sh
+++ scripts/bash_c_build_utils.sh
@@ -15,27 +15,126 @@
 #
 # =================================================================================================
 
+source "./bash_shell_utils.sh"
 source "./bash_console_utils.sh"
 source "./bash_file_system_utils.sh"
 
-# Function to check for cppcheck
-function hasCppcheck () {
-	if cmdInstalled "cppcheck"; then
+# Function to check for clang
+function hasClang () {
+	if cmdInstalled "clang"; then
 		true
 	else
 		false
 	fi
 }
 
-# Function to get cppcheck version
-function cppcheckVersion () {
-	if hasCppcheck
+# Function to get clang version
+function clangVersion () {
+	if hasClang
 	then
-		CPPCHECK_VER="$(cppcheck --version)"
-		if stringBeginsWithSubstring "$CPPCHECK_VER" "Cppcheck "
+		CLANG_VER="$(clang --version)"
+		if isDarwin
 		then
-			CPPCHECK_VER="$(echo $CPPCHECK_VER | cut -d$' ' -f 2)"
-			echo $CPPCHECK_VER
+			if stringBeginsWithSubstring "$CLANG_VER" "Apple clang version "
+			then
+                CLANG_VER=${CLANG_VER#*clang version *}
+                CLANG_VER=${CLANG_VER% (*}
+				echo $CLANG_VER
+			else
+				echo "unknown"
+			fi
+		else
+			if stringBeginsWithSubstring "$CLANG_VER" "clang version "
+            then
+                CLANG_VER=${CLANG_VER#*clang version *}
+                CLANG_VER=${CLANG_VER::6}
+                echo $CLANG_VER
+            else
+                echo "unknown"
+            fi
+		fi
+	else
+		echo "N/A"
+	fi
+}
+
+# Function to get Clang standard C version
+function clangStdCVersion () {
+	if hasClang
+	then
+		CLANG_DEFS=$(clang -dM -E - < /dev/null)
+		C_STD_VERSION=${CLANG_DEFS#*__STDC_VERSION__}
+		C_STD_VERSION=${C_STD_VERSION::7}
+	else
+		echo "N/A"
+	fi
+}
+
+# Function to check for C standard support
+function clangSupportsCStandard () {
+	if hasClang
+	then
+		C_STD_VERSION=0
+		clangStdCVersion
+		C_STD_YEAR=${C_STD_VERSION::5}
+		C_STD_MONTH=${C_STD_VERSION:5:2}
+		if [ "$C_STD_YEAR" -ge "$1" ]; then
+			true
+		else
+			false
+		fi
+	else
+		false
+	fi
+}
+
+# Function to check for clang C99 support
+function clangSupportsC99 () {
+    clangSupportsCStandard "1999"
+}
+
+# Function to check for clang C11 support
+function clangSupportsC11 () {
+    clangSupportsCStandard "2011"
+}
+
+# Function to check for clang C17 support
+function clangSupportsC17 () {
+    clangSupportsCStandard "2017"
+}
+
+# Function to check for G++
+function hasG++ () {
+	if cmdInstalled "g++"; then
+		true
+	else
+		false
+	fi
+}
+
+# Function to check for GCC
+function hasGcc () {
+	if ! isDarwin
+	then
+		if cmdInstalled "gcc"; then
+			true
+		else
+			false
+		fi
+	else
+		false
+	fi
+}
+
+# Function to get GCC version
+function gccVersion () {
+	if hasGcc
+	then
+		GCC_VER="$(gcc --version)"
+		if stringBeginsWithSubstring "$GCC_VER" "gcc "
+		then
+			GCC_VER="$(echo $GCC_VER | cut -d$' ' -f 4)"
+			echo $GCC_VER
 		else
 			echo "unknown"
 		fi
@@ -44,112 +143,163 @@ function cppcheckVersion () {
 	fi
 }
 
-# Function to run cppcheck
-# <name> <path> <verbose> <log> <defines>
-function checkIt () {
-	if hasCppcheck
+# Function to get GCC standard C version
+function gccStdCVersion () {
+	if hasGcc
 	then
-		printIt "Checking $1 with cppcheck..."
-		pushPath "$2" "$3"
-		if [ "$5" == "" ]
-		then
-			cppcheck --inline-suppr addon=cert.py . > "$4" 2>&1
-		else
-			cppcheck --inline-suppr addon=cert.py "$5" . > "$4" 2>&1
-		fi
-		RL_LOG_CONTENTS=`cat "$4"`
-		if stringHasSubstring "$RL_LOG_CONTENTS" "error"
-		then
-			cat "$4"
-			popPath "$3"
-			printError "\tcppcheck of $1 generated errors!"
-			printIt " "
-			exit 1
-		elif stringHasSubstring "$RL_LOG_CONTENTS" "warning"
-		then
-			cat "$4"
-			printWarning "\tcppcheck of $1 generated warnings!"
-            printIt " "
-		else
-			if [ "$3" -eq 1 ]
-			then
-				cat "$4" 
-			fi
-			printSuccess "\tcppcheck of $1 is good."
-			printIt " "
-		fi
-		popPath "$3"
+		GCC_DEFS=$(gcc -dM -E - < /dev/null)
+		C_STD_VERSION=${GCC_DEFS#*__STDC_VERSION__}
+		C_STD_VERSION=${C_STD_VERSION::7}
 	else
-		printWarning "cppcheck doesn't appear to be installed."
+		echo "N/A"
 	fi
 }
 
-# Function to check for scan-build
-function hasScanBuild () {
-    if cmdInstalled "scan-build"; then
-        true
+# Function to check for C standard support
+function gccSupportsCStandard () {
+	if hasGcc
+	then
+		C_STD_VERSION=0
+		gccStdCVersion
+		C_STD_YEAR=${C_STD_VERSION::5}
+		C_STD_MONTH=${C_STD_VERSION:5:2}
+		if [ "$C_STD_YEAR" -ge "$1" ]; then
+			true
+		else
+			false
+		fi
+	else
+		false
+	fi
+}
+
+# Function to check for GCC C99 support
+function gccSupportsC99 () {
+    gccSupportsCStandard "1999"
+}
+
+# Function to check for GCC C11 support
+function gccSupportsC11 () {
+    gccSupportsCStandard "2011"
+}
+
+# Function to check for GCC C17 support
+function gccSupportsC17 () {
+    gccSupportsCStandard "2017"
+}
+
+# Function to check for make
+function hasMake () {
+	if cmdInstalled "make"; then
+		true
+	else
+		false
+	fi
+}
+
+# Function to get make version
+function makeVersion () {
+	if hasMake
+	then
+		MAKE_VER="$(make --version)"
+		if stringBeginsWithSubstring "$MAKE_VER" "GNU Make "
+		then
+            MAKE_VER=${MAKE_VER%Built for*}
+            MAKE_VER=${MAKE_VER%Copyright*}
+            MAKE_VER=${MAKE_VER#GNU Make}
+			echo $MAKE_VER
+		else
+			echo "unknown"
+		fi
+	else
+		echo "N/A"
+	fi
+}
+
+# Function to check for gprof
+function hasGprof () {
+	if cmdInstalled "gprof"; then
+		true
+	else
+		false
+	fi
+}
+
+# Function to get gprof version
+function gprofVersion () {
+    if hasGprof
+    then
+        GPROF_VER="$(gprof --version)"
+        if stringBeginsWithSubstring "$GPROF_VER" "GNU gprof"
+        then   
+            GPROF_VER=${GPROF_VER%Based on*}
+            GPROF_VER=${GPROF_VER#*) }
+            echo $GPROF_VER
+        else
+            echo "Unknown"
+        fi
     else
-        false
+        echo "N/A"
     fi
 }
 
-# Function to run scan-build
+# Function to clean 
 # <name> <path> <makefilename> <verbose> <log>
-function scanIt () {
-	if hasScanBuild
+function cleanIt () {
+	printIt "Cleaning $1..."
+	pushPath "$2" "$4"
+	make clean --makefile="$3" > "$5" 2>&1
+	if [ "$?" -ne 0 ]
 	then
-		printIt "Checking $1 with scan-build..."
-		pushPath "$2" "$4"
-		scan-build make --makefile="$3" > "$5" 2>&1
-		LOG_CONTENTS=`cat $"$5"`
-		if stringHasSubstring "$LOG_CONTENTS" "error"
-		then
-			cat "$5"
-			printError "\tscan-build of $1 generated errors!"
-            printIt " "
-		elif stringHasSubstring "$LOG_CONTENTS" "warning"
-		then
-			cat "$5"
-			printWarning "\tscan-build of $1 generated warnings!"
-            printIt " "
-		else
-			if [ "$4" -eq 1 ]
-			then
-				cat "$5"
-			fi
-			printSuccess "\tscan-build of $1 is good."
-            printIt " "
-		fi
+		cat "$5"
 		popPath "$4"
+		printIt " "
+		printError "\tClean of $1 failed!\n"
+		exit 1
 	else
-		printWarning "scan-build doesn't appear to be installed."
-	fi
-}
-
-# Function to check for valgrind
-function hasValgrind () {
-    if cmdInstalled "valgrind"; then
-        true
-    else
-        false
-    fi
-}
-
-# Function to get valgrind version
-function valgrindVersion () {
-	if hasValgrind
-	then
-		VALGRIND_VER="$(valgrind --version)"
-		if stringBeginsWithSubstring "$VALGRIND_VER" "valgrind-"
+		if [ "$4" -eq 1 ]
 		then
-            VALGRIND_VER=${VALGRIND_VER%.*}
-            VALGRIND_VER=${VALGRIND_VER##*-}
-			echo $VALGRIND_VER
-		else
-			echo "unknown"
+			cat "$5"
 		fi
+        printSuccess "\tClean of $1 succeeded.\n"
+	fi
+	popPath "$4"
+}
+
+# Function to build
+# <name> <path> <makefilename> <verbose> <log> <defines>
+function buildIt () {
+	printIt "Building $1..."
+	pushPath "$2" "$4"
+	if [ "$6" == "" ]
+	then
+		make --makefile="$3" > "$5" 2>&1
 	else
-		echo "N/A"
+		make "$6" --makefile="$3" > "$5" 2>&1
+	fi
+	if [ "$?" -ne 0 ]
+	then	
+		cat "$5"
+		popPath "$4"
+		printIt " "
+		printError "\tBuild of $1 failed!\n"
+		exit 1
+	else
+		if [ "$4" -eq 1 ]
+		then
+			cat "$5"
+		fi
+        printSuccess "\tBuild of $1 succeeded.\n"
+	fi
+	popPath "$4"
+}
+
+# Function to check for Git
+function hasGit () {
+	if cmdInstalled "git"; then
+		true
+	else
+		false
 	fi
 }
 
